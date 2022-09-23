df128e450c22073aa3563df4430bdc82a4df8e8a
kujirahand <web@kujirahand.com>
2022-09-23 12:43:29 +0900

    auto

--- src/ch1/json_write_error.py
+++ src/ch1/json_write_error.py
@@ -1,11 +1,11 @@
 # 不完全なJSON書き込み例
 import json
 
-# [エラー] encodingの指定を忘れている
+# [エラー] encodingの指定を忘れている --- (※1)
 with open('test_error.json', 'w') as fp:
     json.dump('日本語あいうえお', fp, ensure_ascii=False)
 
-# [OK] encodingを正しく指定している
+# [OK] encodingを正しく指定している --- (※2)
 with open('test_ok.json', 'w', encoding='utf-8') as fp:
     json.dump('日本語あいうえお', fp, ensure_ascii=False)
 
