90498ac27ae51479db7922834fd3116a5295cbb3
kujirahand <web@kujirahand.com>
2022-11-16 17:37:37 +0900

    auto

--- src/ch1/linechart_pop.py
+++ src/ch1/linechart_pop.py
@@ -14,8 +14,8 @@ for row in data:
 
 # グラフを描画 --- (※3)
 p_total = plt.plot(x, totals, label='合計(千人)')
-p_woman = plt.plot(x, woman, marker='.', label='女')
 p_man = plt.plot(x, man, marker='x', label='男')
+p_woman = plt.plot(x, woman, marker='.', label='女')
 plt.legend() # 凡例を表示 --- (※4)
 plt.show()
 
