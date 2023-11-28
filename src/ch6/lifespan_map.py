e45b7d39290169a7021b0dd7daa2511e7d73473b
kujirahand <web@kujirahand.com>
2023-11-28 17:34:42 +0900

    lifespan_map.py のエラーを修正

--- src/ch6/lifespan_map.py
+++ src/ch6/lifespan_map.py
@@ -13,6 +13,7 @@ min_val = min([v for k,v in data.items()])
 # 都道府県の平均寿命を色データに変換 --- (※3)
 cmap = plt.get_cmap('coolwarm')
 norm = plt.Normalize(vmin=min_val, vmax=max_val)
+print(norm)
 # 各都道府県の値を処理 --- (※4)
 coldata = {}
 for pname, age in data.items():
@@ -20,6 +21,6 @@ for pname, age in data.items():
     print(pname, age, coldata[pname])
 
 # 日本地図に色を塗る --- (※5)
-plt.colorbar(plt.cm.ScalarMappable(norm, cmap))
+# plt.colorbar(mappable=plt.cm.ScalarMappable(norm=norm, cmap=cmap))
 plt.imshow(japanmap.picture(coldata))
 plt.show()
